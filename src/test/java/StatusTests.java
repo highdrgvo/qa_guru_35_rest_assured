c4f218dad840422b2b900ffc19c229358de0db41
sergeyplechko <sergeyplechko@gmail.com>
2025-06-06 08:27:29 +0300

    added positive and negative tests

--- src/test/java/StatusTests.java
+++ src/test/java/StatusTests.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.is;
 
 
@@ -25,10 +26,35 @@ public class StatusTests {
     @Test
     void checkTotalWithLogs() {
         given()
-                .log().all()
+                .log().all() // можно сделать log.uri
                 .get("https://selenoid.autotests.cloud/status")
                 .then()
-                .log().all()
+                .log().all() // можно сделать log.body
                 .body("total", is(5));
+    }
+
+    @Test
+    void checkStatusCodeWithLogs() {
+        given()
+                .log().uri()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(5));
+    }
+
+    @Test
+    void checkBrowserVersionWithLogs() {
+        given()
+                .log().uri()
+                .get("https://selenoid.autotests.cloud/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("total", is(5))
+                .body("browsers.chrome", hasKey("128.0"));
     }
 }
